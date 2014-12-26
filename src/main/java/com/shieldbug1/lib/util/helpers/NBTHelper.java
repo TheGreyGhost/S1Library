932b7c20adc9417c7674501ee924fc0dbb2d3e9c
Gal Aharon <gal.aharon99@gmail.com>
2014-12-26 16:29:57 +1100

    Added generic set and get methods to NBTHelper
    I was bored, and I felt like I needed a system for getting/setting from NBTTagCompounds in the future, in case it ever changed.

--- src/main/java/com/shieldbug1/lib/util/helpers/NBTHelper.java
+++ src/main/java/com/shieldbug1/lib/util/helpers/NBTHelper.java
@@ -3,36 +3,199 @@ package com.shieldbug1.lib.util.helpers;
 import java.util.UUID;
 
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.*;
 
 /**
  * A class to help with using the Named Binary Tag (NBT) classes.
  */
 public final class NBTHelper
-{
+{	
+	/**
+	 * A simple class to allow for use of generics with helper methods
+	 * {@link #set(NBTTagCompound, String, Object)} an {@link #get(NBTTagCompound, String)}.
+	 * 
+	 * Alternate way: Enum of data types with a switch.
+	 */
+	public static abstract class DataType<T>
+	{
+		public static final DataType<NBTBase> TAG = new DataType<NBTBase>()
+				{
+					@Override
+					public void set(NBTTagCompound compound, String key, NBTBase value)
+					{
+						compound.setTag(key, value);
+					}
+
+					@Override
+					public NBTBase get(NBTTagCompound compound, String key)
+					{
+						return compound.getTag(key);
+					}
+				};
+		public static final DataType<Byte> BYTE = new DataType<Byte>()
+				{
+					@Override
+					public void set(NBTTagCompound compound, String key, Byte value)
+					{
+						compound.setByte(key, value);
+					}
+
+					@Override
+					public Byte get(NBTTagCompound compound, String key)
+					{
+						return compound.getByte(key);
+					}
+				};
+		public static final DataType<Short> SHORT = new DataType<Short>()
+				{
+					@Override
+					public void set(NBTTagCompound compound, String key, Short value)
+					{
+						compound.setShort(key, value);
+					}
+
+					@Override
+					public Short get(NBTTagCompound compound, String key)
+					{
+						return compound.getShort(key);
+					}
+				};
+		public static final DataType<Integer> INTEGER = new DataType<Integer>()
+				{
+					@Override
+					public void set(NBTTagCompound compound, String key, Integer value)
+					{
+						compound.setInteger(key, value);
+					}
+		
+					@Override
+					public Integer get(NBTTagCompound compound, String key)
+					{
+						return compound.getInteger(key);
+					}
+				};
+		public static final DataType<Long> LONG = new DataType<Long>()
+				{
+					@Override
+					public void set(NBTTagCompound compound, String key, Long value)
+					{
+						compound.setLong(key, value);
+					}
+		
+					@Override
+					public Long get(NBTTagCompound compound, String key)
+					{
+						return compound.getLong(key);
+					}
+				};
+		public static final DataType<Float> FLOAT = new DataType<Float>()
+				{
+					@Override
+					public void set(NBTTagCompound compound, String key, Float value)
+					{
+						compound.setFloat(key, value);
+					}
+		
+					@Override
+					public Float get(NBTTagCompound compound, String key)
+					{
+						return compound.getFloat(key);
+					}
+				};
+		public static final DataType<Double> DOUBLE = new DataType<Double>()
+				{
+					@Override
+					public void set(NBTTagCompound compound, String key, Double value)
+					{
+						compound.setDouble(key, value);
+					}
+
+					@Override
+					public Double get(NBTTagCompound compound, String key)
+					{
+						return compound.getDouble(key);
+					}
+				};
+		public static final DataType<String> STRING = new DataType<String>()
+				{
+					@Override
+					public void set(NBTTagCompound compound, String key, String value)
+					{
+						compound.setString(key, value);
+					}
+		
+					@Override
+					public String get(NBTTagCompound compound, String key)
+					{
+						return compound.getString(key);
+					}
+				};
+		public static final DataType<byte[]> BYTE_ARRAY = new DataType<byte[]>()
+				{
+					@Override
+					public void set(NBTTagCompound compound, String key, byte[] value)
+					{
+						compound.setByteArray(key, value);
+					}
+		
+					@Override
+					public byte[] get(NBTTagCompound compound, String key)
+					{
+						return compound.getByteArray(key);
+					}
+				};
+		public static final DataType<int[]> INT_ARRAY = new DataType<int[]>()
+				{
+					@Override
+					public void set(NBTTagCompound compound, String key, int[] value)
+					{
+						compound.setIntArray(key, value);
+					}
+		
+					@Override
+					public int[] get(NBTTagCompound compound, String key)
+					{
+						return compound.getIntArray(key);
+					}
+				};
+		public static final DataType<Boolean> BOOLEAN = new DataType<Boolean>()
+				{
+					@Override
+					public void set(NBTTagCompound compound, String key, Boolean value)
+					{
+						compound.setBoolean(key, value);
+					}
+						
+					@Override
+					public Boolean get(NBTTagCompound compound, String key)
+					{
+						return compound.getBoolean(key);
+					}
+				};
+		public static final DataType<NBTTagCompound> COMPOUND_TAG = new DataType<NBTTagCompound>()
+				{
+					@Override
+					public void set(NBTTagCompound compound, String key, NBTTagCompound value)
+					{
+						compound.setTag(key, value);
+					}
+						
+					@Override
+					public NBTTagCompound get(NBTTagCompound compound, String key)
+					{
+						return compound.getCompoundTag(key);
+					}
+				};
+		private DataType()
+		{
+			
+		}
+		
+		public abstract void set(NBTTagCompound compound, String key, T value);
+		public abstract T get(NBTTagCompound compound, String key);
+	}
+	
 	private NBTHelper(){}
-	
-	/**
-	 * Sets a tag to another tag.
-	 * @param compound - the tag to append <i>to.<i>
-	 * @param key - the key to put the tag under.
-	 * @param value - the tag to put in the key.
-	 */
-	public static void setTag(NBTTagCompound compound, String key, NBTTagCompound value)
-	{
-		compound.setTag(key, compound);
-	}
-	
-	/**
-	 * Retrieves a tag from another tag.
-	 * @param compound - tag to get from.
-	 * @param key - key to get tag from.
-	 * @return the tag if there is one, otherwise it returns a new NBTTagCompound.
-	 */
-	public static NBTTagCompound getTag(NBTTagCompound compound, String key)
-	{
-		return compound.getCompoundTag(key);
-	}
 	
 	/**
 	 * Creates an NBTTagCompound containing information about an ItemStack.
@@ -90,5 +253,28 @@ public final class NBTHelper
 			stack.setTagCompound(new NBTTagCompound());
 		}
 		return stack.getTagCompound();
+	}
+	
+	/**
+	 * Sets a value in a non-null NBTTagCompound.
+	 * @param type - the type of data to set.
+	 * @param compound - the NBTTagCompound to write to.
+	 * @param key - the key to write to.
+	 * @param value - the value to set.
+	 */
+	public static <T> void set(DataType<T> type, NBTTagCompound compound, String key,  T value)
+	{
+		type.set(compound, key, value);
+	}
+	
+	/**
+	 * Gets a value from a non-null NBTTagCompound.
+	 * @param type - the type of data to get.
+	 * @param compound - the NBTTagCompound to get from.
+	 * @param key - the key to read from.
+	 */
+	public static <T> T get(DataType<T> type, NBTTagCompound compound, String key)
+	{
+		return type.get(compound, key);
 	}
 }
